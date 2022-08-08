7aae90db3f28cd61d2e2aa8cccd4a910dbbfc092
Slav <itiksolution@gmail.com>
2022-08-08 21:55:04 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    name[i] = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+  return name
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'surpise'))
+
+/*--------------- Optional ------------------------*/
+// const newArr = ["Guadalupe", "Ollie", "Aki"]
+
+// function writeCards(name, event) {
+//   for (let i = 0; i < name.length; i++) {
+//     newArr[i] = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+//   }
+//   return newArr
+// }
+// console.log(writeCards(newArr, 'surpise'))
+
+function countDown(params) {
+  let i = 10
+  while (i >= 0) {
+    console.log(i--)
+  }
+  return
+}
+countDown()
